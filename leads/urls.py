62d25e6bd81bf9a88b61ff1260a1c596556b1b0d
safal-dev <info.safalpoudel@gmail.com>
2025-07-31 07:20:21 +0545

    changes on history

--- leads/urls.py
+++ leads/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('delete_instruction/<int:pk>/', views.delete_instruction, name='delete_instruction'),
     path('set_default_instruction/<int:pk>/', views.set_default_instruction, name='set_default_instruction'),
     path('download/<str:filename>/', views.download_processed_file, name='download_processed_file'),
+    path('history/', views.history_view, name='history'),
 ]
